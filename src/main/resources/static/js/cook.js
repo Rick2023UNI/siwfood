757026a3c997339448baedca85eb90c3c9b6221b
DESKTOP-KGOG9E1\Dragon <ric.cacciamani@stud.uniroma3.it>
2024-06-30 15:12:42 +0200

    Possibilità di condividere un cuoco

---
+++ src/main/resources/static/js/cook.js
@@ -0,0 +1,25 @@
+function share(event) {
+	var linkToShare=window.location.href;
+	var text="Ricette del cuoco "+document.getElementsByTagName("h1")[0].textContent+" ";
+	var action=event.currentTarget.id; 
+	var link="";
+	if (action=="facebook") {
+		link='https://www.facebook.com/sharer/sharer.php?u='+linkToShare;
+	} 
+	else if (action=="whatsapp") {
+		link='https://wa.me/?text='+text+" "+linkToShare;
+	} 
+	else if (action=="twitter") {
+		link="https://twitter.com/intent/tweet?url="+linkToShare+"&text="+text;
+	}
+	else if (action=="reddit") {
+		link="http://www.reddit.com/submit?url="+linkToShare;
+	}
+	else if (action=="linkedin") {
+		link="https://www.linkedin.com/shareArticle?mini=true&amp;url="+linkToShare+"&amp;title="+text+"&summary="+text+linkToShare+";source=";
+	}
+	else if (action=="email") {
+		link="mailto:?subject="+text+"&body="+text+linkToShare;
+	}
+	window.open(link, 'pop-up', 'left=20,top=20,width=500,height=500,toolbar=1,resizable=0'); 
+}
